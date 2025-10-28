cb0389e56717caa800c1757279824af80e646dec
Yukih <158501974+Y-u-k-i-h@users.noreply.github.com>
2025-10-28 10:55:41 +0300

    spring boot init

---
+++ src/main/java/org/devconnect/devconnectbackend/DevConnectBackendApplication.java
@@ -0,0 +1,13 @@
+package org.devconnect.devconnectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevConnectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevConnectBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/devconnect/devconnectbackend/DevConnectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.devconnect.devconnectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevConnectBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
